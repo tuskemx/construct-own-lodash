075ffb77c778c2fcc03577b7e5fd844b9836192c
Tuskemx <ma.edwards@live.com>
2019-04-11 12:55:29 +0100

    finished fill lowbar

--- lowbar.js
+++ lowbar.js
@@ -4,4 +4,39 @@ _.identity = (x) => {
   return x;
 };
 
+
+_.fromPairs = (array) => {
+  if (array.length === 0) return {};
+  const obj = {}
+  for (let i = 0; i < array.length; i++) {
+     obj[array[i][0]] = array[i][1];
+  }
+  return obj;
+}
+
+_.fill = (array, value, start, end) => {
+  console.log(array);
+  console.log(value);
+  console.log(start);
+  console.log(end);
+  
+  for (let i = start; i < end; i++) {
+    array.push(value);
+  }
+  console.log(value);
+  console.log(start);
+  console.log(end);
+  console.log(array);
+  return array;
+   
+}
+
+
+
+
+
+
+
+
+
 module.exports = _;
--- spec/identity.spec.js
+++ spec/identity.spec.js
@@ -13,3 +13,38 @@ describe('#identity', () => {
     expect(_.identity(arr)).to.equal(arr);
   });
 });
+
+describe('fill', () => {
+  it('returns an empty object when passed an empty array', () => {
+    const input = [];
+    const actualResult = _.fromPairs(input);
+    const desiredResult = {};
+    expect(actualResult).to.eql(desiredResult);
+  });
+  it('expect one nested array to return object with key value', () => {
+    const input = [['a', 3]];
+    const actualResult = _.fromPairs(input);
+    const desiredResult = {'a': 3};
+    expect(actualResult).to.eql(desiredResult);
+  });
+  it('expect one nested array to return object with key value', () => {
+    const input = [['a', 3], ['b', 4], ['c', 7]];
+    const actualResult = _.fromPairs(input);
+    const desiredResult = {'a': 3, 'b': 4, 'c': 7};
+    expect(actualResult).to.eql(desiredResult);
+
+  });
+});
+
+describe('fill', () => {
+  it('returns an empty object when passed an empty array', () => {
+    const actualResult = _.fill([], '', 0, 0);
+    const desiredResult = [];
+    expect(actualResult).to.eql(desiredResult);
+  });
+  it('takes empty array and flls with value between start end values', () => {
+    let actualResult = _.fill([], 'a', 0, 3);
+    let desiredResult = ['a', 'a', 'a'];
+    expect(actualResult).to.eql(desiredResult);
+});
+});
